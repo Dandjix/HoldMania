e69e53decfa62066aab2bbe19e6be17ddde6eaf6
Timon <timondubreuil@outlook.com>
2024-12-03 16:23:55 +0100

    Initial Commit

---
+++ HoldMania/HoldManiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoldManiaApp.swift
+//  HoldMania
+//
+//  Created by dubreuil timon on 03/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct HoldManiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
